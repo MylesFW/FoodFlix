b47d43ea35c741b37a153e1f778f378e40dd8a47
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-10-19 12:32:36 -0700

    Added react bootstrap and added Navbar.js

--- food_flix/src/App.js
+++ food_flix/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalComponent from './components/TestFunctional';
 import TestClass from './components/TestClass';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 	return (
---
+++ food_flix/src/components/Navbar.js
@@ -0,0 +1,12 @@
+function Navbar() {
+	return (
+		<div>
+      
+
+			<TestClass test_prop={'test text'} />
+			<FunctionalComponent test_num={10} test_string="tester" />
+		</div>
+	);
+}
+
+export default Navbar;
