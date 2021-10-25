73690e09c55dc6f89c4c246bbf40160ba8cbcd9e
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-10-25 15:55:54 -0700

    Added Card api still making call but card is not fully mapping

--- food_flix/src/components/Body.js
+++ food_flix/src/components/Body.js
@@ -1,56 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Col,Container,Row } from 'react-bootstrap';
+import { Col,Container,Row,Card,Button } from 'react-bootstrap';
 
 const Movie = (props) => {
 	//Note the second argument is an empty array. We are only
 	//replaced null with empty arr
 	const [movie, setMovie] = useState([]);
 	console.log(movie);
-	console.log(setMovie);
+  const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=';
+  //const IMG_PATH = 'https//image.tmdb.org/t/p/w1280-';
+
 
 	useEffect(() => {
 		axios
 			.get(
-        'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key='+
-				process.env.REACT_APP_FOODFLIX_API_KEY+'&page=1'
+        API_URL +
+				process.env.REACT_APP_FOODFLIX_API_KEY
 			)
 			.then((res) => {
 				setMovie(res.data.results);
-				console.log(res);
 			})
 			//  .then(response=>{setMovie(response.data)})
 			//  })
 			.catch((err) => {
 				console.log(err);
 			});
-	}, []);
+	},[]);
 
   if (movie === null) {
-    return <img src ="https://cdn.dribbble.com/users/576558/screenshots/3801681/media/f2148dc5132ea854540a3c693bc51d5d.png?compress=1&resize=800x600"></img>
+    return <img alt="movie" src ="https://cdn.dribbble.com/users/576558/screenshots/3801681/media/f2148dc5132ea854540a3c693bc51d5d.png?compress=1&resize=800x600" />
   }
-
-	return (
-		<>
-			
-      <Container>
-    <h1>Movie Title</h1>
-    <Row>
-		
   
-      <Col>	{movie.length > 0 &&
-				movie.map((movie, index) => {
-					return <div key={index}>{movie.title}</div>;
-				})} </Col>
-      
-    </Row>
-    <Row>
-      <Col>1 of 3</Col>
-      <Col>2 of 3</Col>
-      <Col>3 of 3</Col>
-    </Row>
-  </Container>
-
+	return (
+    <>
+    <Container>
+    <h1>Movie Title</h1>	
+		<Row>
+			<Col> 
+				<Card style={{ width: '18rem' }}>
+				{movie.map((titles) => {
+					return
+					<div>
+          <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+								<Card.Title key="movie">{titles.title}</Card.Title>
+								<Card.Text> {titles.overview}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+								</Card.Body>
+								</div>
+							})}
+            
+        </Card>
+      </Col>
+    </Row>     
+    </Container>
 		</>
 	);
 };
